9d231a3aabb3604479b842f219a0b0b3f36a5962
Antoine d'Otreppe <a.dotreppe@aspyct.org>
2014-10-15 23:18:59 +0200

    Initial Commit

---
+++ kmlparser/kmlparser.h
@@ -0,0 +1,19 @@
+//
+//  kmlparser.h
+//  kmlparser
+//
+//  Created by Antoine d'Otreppe on 15/10/14.
+//  Copyright (c) 2014 Aspyct.org. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for kmlparser.
+FOUNDATION_EXPORT double kmlparserVersionNumber;
+
+//! Project version string for kmlparser.
+FOUNDATION_EXPORT const unsigned char kmlparserVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <kmlparser/PublicHeader.h>
+
+
